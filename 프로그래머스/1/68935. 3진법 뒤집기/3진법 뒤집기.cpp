335638e82e416dc987add92b2e57be9890a3ccf3
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-11-14 04:28:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -5,25 +5,14 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0, index = 0;
+    int answer = 0;
     vector<int> threes;
     
-    for (int i = 0; ; i++) {
-        if (pow(3, i) > n) {
-            index = i - 1;
-            break;
-        }
-        else if (pow(3, i) == n) {
-            index = i;
-            break;
-        }
-    }
-    while (n > 0) {
-        threes.push_back(n / (int)pow(3, index));
-        n %= (int)pow(3, index);
-        index--;
+    while (n) {
+        threes.push_back(n % 3);
+        n /= 3;
     }
     for (int i = 0; i < threes.size(); i++)
-        answer += pow(3, i) * threes[i];
+        answer += pow(3, i) * threes[threes.size() - 1 - i];
     return answer;
 }
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.67 MB, 시간: 0.03 ms
+메모리: 3.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 4:9:26
+2023년 11월 2일 4:28:1
 
 ### 문제 설명
 
