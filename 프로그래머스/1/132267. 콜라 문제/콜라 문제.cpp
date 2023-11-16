1e7be6e4fca500b58c48d708405fe7e25df1583a
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-11-16 22:17:22 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -3,34 +3,11 @@
 
 using namespace std;
 
-int leftOver = 0;
-
-int CalculateBottle(int emptyCoke, int rewardCoke, int n) {
-    int count = 0;
-    
-    while (n > 0) {
-        while (n % emptyCoke) {
-            leftOver++;
-            n--;
-        }
-        n = (n / emptyCoke) * rewardCoke;
-        count += n;
-    }
-    return count;
-}
-
 int solution(int emptyCoke, int rewardCoke, int n) {
-    int answer = 0, amount = 0;
-    
-    while (true) {
-        amount = CalculateBottle(emptyCoke, rewardCoke, n);
-        
-        if (amount) {
-            answer += amount;
-            n = leftOver;
-            leftOver = 0;
-        }
-        else break;
+    int answer = 0;
+    while (n >= emptyCoke) {
+        answer += rewardCoke;
+        n -= emptyCoke - rewardCoke; // n = n - emptyCoke + rewardCoke;
     }
     return answer;
 }
