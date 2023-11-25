be16445f2d1733172c44ad2f86452a705aa1d1fb
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-11-25 17:23:46 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    int a, b;
+    int answer = 0;
+    
+    cin >> a >> b;
+
+    while (a < b) {
+        if (b % 10 == 1) b /= 10;
+        else if (b % 2 == 0) b /= 2;
+        else break;
+        answer++;
+    }
+    answer++;
+    if (a != b) answer = -1;
+    cout << answer;
+    return 0;
+}
