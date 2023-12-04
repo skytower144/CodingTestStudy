883d5caaa48e371759895ecead56604589096141
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-04 22:08:23 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 124 ms, Memory: 7264 KB -BaekjoonHub

---
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int k, w, h;
+int grid[200][200];
+int visited[200][200][30];
+int dx[] = { 0, 0,-1, 1, 1, 2, 2, 1,-1,-2,-2,-1};
+int dy[] = {-1, 1, 0, 0,-2,-1, 1, 2, 2, 1,-1,-2};
+int nx, ny;
+
+int FindPath(int x, int y) {
+    visited[y][x][0] = 1;
+    queue<vector<int>> q;
+    q.push({x, y, 0});
+
+    vector<int> pos;
+    int skillCount;
+
+    while (!q.empty()) {
+        pos = q.front();
+        q.pop();
+
+        x = pos[0];
+        y = pos[1];
+        skillCount = pos[2];
+
+        if (x == w - 1 && y == h - 1)
+            return visited[h - 1][w - 1][skillCount] - 1;
+
+        int endIdx = 12;
+        if (skillCount >= k)
+            endIdx = 4;
+
+        for (int i = 0; i < endIdx; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+
+            if (nx >= 0 && nx < w && ny >= 0 && ny < h && grid[ny][nx] == 0) {
+                if (skillCount < k && i >= 4 && !visited[ny][nx][skillCount + 1]) {
+                    visited[ny][nx][skillCount + 1] = visited[y][x][skillCount] + 1;
+                    q.push({nx, ny, skillCount + 1});
+                }
+                else if (i < 4 && !visited[ny][nx][skillCount]){
+                    visited[ny][nx][skillCount] = visited[y][x][skillCount] + 1;
+                    q.push({nx, ny, skillCount});
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> k >> w >> h;
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++)
+            cin >> grid[i][j];
+    }
+    int answer = FindPath(0, 0);
+    cout << answer;
+    return 0;
+}
