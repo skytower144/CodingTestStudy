70b7b8b99f23ba51ee7748ddfe69c1b40c1c601b
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-14 23:43:19 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -8,21 +9,28 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     
-    int n, m;
+    int n, m, num;
     cin >> n >> m;
 
-    vector<long long> cards(n, 0);
-    for (int i = 0; i < n; i++) cin >> cards[i];
+    priority_queue<long long> pq;
+    long long card1, card2, result;
 
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        pq.push(num * -1);
+    }
     for (int i = 0; i < m; i++) {
-        sort(cards.begin(), cards.end());
-        long long result = cards[0] + cards[1];
-        cards[0] = cards[1] = result;
+        card1 = pq.top() * -1, pq.pop();
+        card2 = pq.top() * -1, pq.pop();
+        result = card1 + card2;
+        pq.push(result * -1);
+        pq.push(result * -1);
     }
     long long answer = 0;
-    for (int i = 0; i < n; i++)
-        answer += cards[i];
-    
+    for (int i = 0; i < n; i++) {
+        answer += pq.top() * -1;
+        pq.pop();
+    }
     cout << answer;
     return 0;
 }
