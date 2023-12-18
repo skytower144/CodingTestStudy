f3100f94dbcd058260d9d36d8047e65cc416e36f
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-18 13:05:48 +0900

    [Gold III] Title: 크게 만들기, Time: 8 ms, Memory: 3620 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, k;
+    string num;
+    string answer = "";
+    cin >> n >> k >> num;
+
+    for (int i = 0; i < n; i++) {
+        while (!answer.empty() && k > 0 && answer.back() < num[i]) {
+            answer.pop_back();
+            k--;
+        }
+        answer += num[i];
+    }
+    while (k) {
+        answer.pop_back();
+        k--;
+    }
+    cout << answer;
+    return 0;
+}
