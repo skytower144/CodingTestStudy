7ed858010581c395b45e27a0d8223a77d137131a
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-19 15:41:41 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<long long> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (int i = 3; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+    cout << dp[n];
+    return 0;
+}
