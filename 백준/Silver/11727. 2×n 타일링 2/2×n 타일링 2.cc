c2ecdb18862d403c1c30730ec6a36d51a6d18b65
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-19 14:49:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+
+    cout << dp[n];
+    return 0;
+}
