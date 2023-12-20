5919e7915e279a9454c0920ca431e9f5d6e32bcd
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-20 15:44:09 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n % 2) cout << "SK";
+    else cout << "CY";
+
+    return 0;
+}
