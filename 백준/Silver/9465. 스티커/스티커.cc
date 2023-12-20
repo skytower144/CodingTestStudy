0e2f4c9d19748882aeffb8eebe8cf5a50b0861bb
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-20 14:34:42 +0900

    [Silver I] Title: 스티커, Time: 92 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -13,32 +13,23 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        
-        vector<vector<int>> sticker(2, vector<int>(n, 0));
+
+        vector<vector<int>> sticker(2, vector<int>(n + 1, 0));
+        vector<vector<int>> dp(2, vector<int>(n + 1, 0));
+
         for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < n; j++)
+            for (int j = 1; j <= n; j++)
                 cin >> sticker[i][j];
         }
-        int maxValue = 0;
-        vector<vector<int>> dp(2, vector<int>(n, 0));
-        
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < 2; i++) {
-                if (j < n - 1) {
-                    dp[(i + 1) % 2][j + 1] = max(maxValue, dp[(i + 1) % 2][j + 1]);
-                    dp[(i + 1) % 2][j + 1] = max(dp[(i + 1) % 2][j + 1], dp[i][j] + sticker[i][j]);
-                }
-            }
-            maxValue = max(dp[0][min(n - 1, j + 1)], dp[1][min(n - 1, j + 1)]);
+        dp[0][1] = sticker[0][1];
+        dp[1][1] = sticker[1][1];
 
-            // cout << "========================\n";
-            // for (int i = 0; i < 2; i++) {
-            //     for (int j = 0; j < n; j++)
-            //         cout << dp[i][j] << " ";
-            //     cout << endl;
-            // }
+        for (int j = 2; j <= n; j++) {
+            dp[0][j] = max(dp[1][j - 1], dp[1][j - 2]) + sticker[0][j];
+            dp[1][j] = max(dp[0][j - 1], dp[0][j - 2]) + sticker[1][j];
         }
-        cout << max(dp[0][n - 1] + sticker[0][n - 1], dp[1][n - 1] + sticker[1][n - 1]) << '\n';
+        cout << max(dp[0][n], dp[1][n]) << '\n';
     }
+
     return 0;
 }
