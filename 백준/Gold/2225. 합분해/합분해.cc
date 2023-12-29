3094899948c5d0ab428e4de89fb0929f819a891d
skytower144 <98079532+skytower144@users.noreply.github.com>
2023-12-29 17:33:37 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -11,23 +11,18 @@ int main() {
     cin >> n >> k;
 
     int dp[201][201] = {0,};
+    
+    for (int i = 0; i <= n; i++) 
+        dp[1][i] = 1;
+    
+    for (int j = 1; j <= k; j++)
+        dp[j][0] = 1;
 
-    for (int i = 1; i <= k; i++) {
-        dp[0][i] = 1;
-        dp[1][i] = i;
-    }
-    for (int j = 2; j <= n; j++) {
-        for (int i = 1; i <= k; i++) {
-            if (i == 1) {
-                dp[j][i] = 1;
-                continue;
-            }
-            for (int l = 0; l <= j; l++) {
-                dp[j][i] += dp[j - l][i - 1] * dp[l][1];
-                dp[j][i] %= 1000000000;
-            }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 2; j <= k; j++) {
+            dp[j][i] = (dp[j][i - 1] + dp[j - 1][i]) % 1000000000;
         }
     }
-    cout << dp[n][k];
+    cout << dp[k][n];
     return 0;
 }
