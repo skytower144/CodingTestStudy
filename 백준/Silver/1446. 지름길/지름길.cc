1fb45315d2b08b0800afbff108aa7f5974673988
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-01-02 19:52:29 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.cc
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -1,34 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 #include <climits>
 
 using namespace std;
-
-void Dijkstra(int start, vector<vector<pair<int, int>>>& roads, vector<int>& distance) {
-    priority_queue<pair<int, int>> pq;
-    pq.push({0, start});
-    distance[start] = 0;
-
-    while (!pq.empty()) {
-        int distanceSum = -pq.top().first;
-        int currentPos = pq.top().second;
-        pq.pop();
-
-        if (distanceSum > distance[currentPos])
-            continue;
-        
-        for (const auto [d, nextPos] : roads[currentPos]) {
-            int nextDistance = d + distanceSum;
-
-            if (nextDistance < distance[nextPos]) {
-                distance[nextPos] = nextDistance;
-                pq.push({-nextDistance, nextPos});
-            }
-        }
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
@@ -37,18 +12,20 @@ int main() {
     cin >> n >> d;
 
     vector<vector<pair<int, int>>> roads(10001);
-    vector<int> distance(10001, INT_MAX);
-    int shortStart, shortEnd, length;
+    vector<int> dp(10001, INT_MAX);
+    int shortStart, shortEnd, distance;
 
-    for (int i = 0; i < d; i++)
-        roads[i].push_back({1, i + 1});
-     
     for (int i = 0; i < n; i++) {
-        cin >> shortStart >> shortEnd >> length;
-        roads[shortStart].push_back({length, shortEnd});
+        cin >> shortStart >> shortEnd >> distance;
+        roads[shortStart].push_back({distance, shortEnd});
     }
-    Dijkstra(0, roads, distance);
-    cout << distance[d];
+    dp[0] = 0;
+    for (int i = 0; i < d; i++) {
+        dp[i + 1] = min(dp[i + 1], dp[i] + 1);
 
+        for (const auto [d, pos] : roads[i])
+            dp[pos] = min(dp[pos], dp[i] + d);
+    }
+    cout << dp[d];
     return 0;
 }
