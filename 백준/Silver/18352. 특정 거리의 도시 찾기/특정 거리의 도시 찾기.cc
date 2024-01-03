2d5bf7df607180f90e2d29f670f6b1ea55e919eb
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-01-03 15:54:05 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 424 ms, Memory: 27896 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -3,11 +3,15 @@
 #include <algorithm>
 #include <queue>
 #include <climits>
+#include <set>
 
 using namespace std;
 
+int n, m, k, x;
+
 void Dijkstra(int start, vector<vector<pair<int, int>>>& graph, vector<int>& distance) {
     priority_queue<pair<int, int>> pq;
+    set<int> cities;
     pq.push({0, start});
     distance[start] = 0;
 
@@ -18,6 +22,9 @@ void Dijkstra(int start, vector<vector<pair<int, int>>>& graph, vector<int>& dis
 
         if (distanceSum > distance[currentCity])
             continue;
+
+        if (distance[currentCity] == k)
+            cities.insert(currentCity);
         
         for (const auto [d, nextCity] : graph[currentCity]) {
             int nextDistance = d + distanceSum;
@@ -28,12 +35,13 @@ void Dijkstra(int start, vector<vector<pair<int, int>>>& graph, vector<int>& dis
             }
         }
     }
+    for (auto c : cities) cout << c << '\n';
+    if (!cities.size()) cout << -1;
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    int n, m, k, x;
     cin >> n >> m >> k >> x;
 
     vector<vector<pair<int, int>>> graph(n + 1);
@@ -45,14 +53,5 @@ int main() {
         graph[startCity].push_back({1, endCity});
     }
     Dijkstra(x, graph, distance);
-    bool flag = false;
-
-    for (int i = 1; i <= n; i++) {
-        if (distance[i] == k) {
-            cout << i << '\n';
-            flag = true;
-        }
-    }
-    if (!flag) cout << -1;
     return 0;
 }
