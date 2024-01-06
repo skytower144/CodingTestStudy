006c679bf8c1acd2522de8dea0368f9f24801c8b
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-01-06 21:25:58 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 92 ms, Memory: 5152 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -37,8 +37,8 @@ int main() {
         int totalDistance;
         bool flag = false;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
                     if (mbti[i] == mbti[j] && mbti[j] == mbti[k])
                         totalDistance = 0;
