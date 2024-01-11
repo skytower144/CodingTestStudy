c788d029e63e12548a15eac7068c8a7ba925326c
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-01-11 14:49:33 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -27,21 +27,13 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     cin >> n >> m;
-
     int a, b;
-    vector<pair<int, int>> edges(m + 1);
 
     for (int i = 0; i <= n; i++)
         parent.push_back(i);
-
+    
     for (int i = 1; i <= m; i++) {
         cin >> a >> b;
-        edges[i] = {a, b};
-    }
-    for (int i = 1; i <= m; i++) {
-        a = edges[i].first;
-        b = edges[i].second;
-
         if (FindParent(a) != FindParent(b))
             UnionParent(a, b);
         else {
