6faf5eb76fcdff499f696098a274afcc1af79113
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-01-14 17:01:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, m, a, b;
+    cin >> n >> m;
+
+    vector<int> numbers(n + 1, 0);
+    vector<int> dp(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> numbers[i];
+        dp[i] = dp[i - 1] + numbers[i];
+    }
+    while (m--) {
+        cin >> a >> b;
+        cout << dp[b] - dp[a - 1] << '\n';
+    }
+    return 0;
+}
