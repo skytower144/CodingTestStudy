980a09300c7ea8d5af2a56397741487ecb07e566
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-01-14 15:08:01 +0900

    [Silver V] Title: 집합, Time: 756 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define ALL 1048575
+
+using namespace std;
+
+int Add(int result, int num) {
+    return result | (1 << (num - 1));
+}
+int Remove(int result, int num) {
+    int filter = ~(1 << (num - 1));
+    return result & filter;
+}
+bool Check(int result, int num) {
+    return result & (1 << (num - 1));
+}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int m, num, result = 0;
+    string command;
+
+    cin >> m;
+    cin.ignore();
+
+    while (m--) {
+        cin >> command;
+
+        if (command == "all")
+            result = ALL;
+        
+        else if (command == "empty")
+            result = 0;
+
+        else {
+            cin >> num;
+
+            if (command == "add")
+                result = Add(result, num);
+
+            else if (command == "remove")
+                result = Remove(result, num);
+
+            else if (command == "check")
+                cout << Check(result, num) << '\n';
+
+            else if (command == "toggle")
+                result = Check(result, num) ? Remove(result, num) : Add(result, num);
+        }
+    }
+    return 0;
+}
