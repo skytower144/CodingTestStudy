bf6fc908b8baae5dd3065c1334c74371a9e7b35e
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-02-26 14:08:44 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int y, m;
+    cin >> y >> m;
+
+    cout << 2 * m - y;   
+
+    return 0;
+}
