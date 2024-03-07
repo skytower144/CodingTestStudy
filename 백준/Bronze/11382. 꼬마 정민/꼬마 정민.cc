033d45c8af8f387c29bc4eb1b8325f3922ca57c8
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-03-07 11:43:09 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+    return 0;
+}
