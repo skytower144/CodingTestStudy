6808a1627f3989e27586e7ae83b5a6d38abd157d
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-03-25 14:03:52 +0900

    [Gold III] Title: 소수의 연속합, Time: 32 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -5,21 +5,22 @@
 
 using namespace std;
 
+bool isntPrime[4000001];
+
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
 
     int n;
     cin >> n;
 
-    vector<int> isPrime(n + 1, 1);
     vector<int> primes;
     
     for (int i = 2; i <= n; i++) {
-        if (!isPrime[i]) continue;
+        if (isntPrime[i]) continue;
         primes.push_back(i);
         
         for (int j = 2; i * j <= n; j++)
-            isPrime[i * j] = 0;
+            isntPrime[i * j] = 1;
     }
     int left = 0;
     int right = 0;
