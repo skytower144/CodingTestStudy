756d4b1f320113c40bb4fe88e8e0448cdd85e7a1
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-03-26 16:16:02 +0900

    [Gold IV] Title: LCS 2, Time: 8 ms, Memory: 10008 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    string a, b;
+    cin >> a >> b;
+
+    int aSize = a.size();
+    int bSize = b.size();
+
+    vector<vector<pair<int, char>>> dp(bSize + 1, vector<pair<int, char>>(aSize + 1));
+
+    for (int i = 0; i < bSize; i++) {
+        for (int j = 0; j < aSize; j++) {
+            if (a[j] == b[i])
+                dp[i + 1][j + 1] = {dp[i][j].first + 1, a[j]};
+            
+            else
+                dp[i + 1][j + 1] = dp[i][j + 1].first < dp[i + 1][j].first ? dp[i + 1][j] : dp[i][j + 1];
+        }
+    }
+    int x = aSize;
+    int y = bSize;
+    string answer = "";
+
+    while (x >= 1 && y >= 1 && dp[y][x].first) {
+        if (dp[y][x].first == dp[y - 1][x].first)
+            y--;
+        
+        else if (dp[y][x].first == dp[y][x - 1].first)
+            x--;
+        
+        else {
+            answer = dp[y][x].second + answer;
+            y--;
+            x--;
+        }
+    }
+    cout << dp[bSize][aSize].first << '\n';
+
+    if (dp[bSize][aSize].first)
+        cout << answer;
+
+    return 0;
+}
