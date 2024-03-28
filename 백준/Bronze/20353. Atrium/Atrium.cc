b25e6e57dca05d2a3c3670e7cbf9b4cded07c5b7
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-03-28 23:29:08 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    double area;
+    cin >> area;
+
+    double side = sqrt(area);
+    double answer = side * 4;
+
+    cout << fixed;
+    cout.precision(6);
+
+    cout << answer;
+
+    return 0;
+}
