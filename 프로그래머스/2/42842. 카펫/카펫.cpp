96f734a84cf9900261c3187a2c1b56b88e1b1d22
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-04-24 16:12:01 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -4,21 +4,12 @@
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    int totalBlocks = brown + yellow;
-    int right = totalBlocks;
-    int left = 1;
+    int h = 3;
+    int w = (brown / 2) - 1;
     
-    while (left <= right) {
-        int frame = (left + right) * 2 - 4;
-        
-        if (frame == brown)
-            return {right, left};
-        
-        while (left++) {
-            if (totalBlocks % left == 0) {
-                right = totalBlocks / left;
-                break;
-            }
-        }
+    while (w * h != brown + yellow) {
+        w--;
+        h++;
     }
+    return {w, h};
 }
