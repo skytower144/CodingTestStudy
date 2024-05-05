98d4e15265dd77dc02589f0ed495ae7dae46bfa5
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-05-05 15:44:17 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -5,15 +5,11 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    
     sort(citations.begin(), citations.end(), greater());
     
     for (int i = 1; i <= citations.size(); i++) {
-        if (i <= citations[i - 1])
-            answer++;
-        else
-            break;
+        if (i > citations[i - 1])
+            return i - 1;
     }
-    return answer;
+    return citations.size();
 }
