04046a6e38e56b2a8c1724d007fee5bb374b3341
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-05-18 11:44:41 +0900

    [Gold III] Title: 전기가 부족해, Time: 48 ms, Memory: 8692 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
@@ -6,58 +6,46 @@
 using namespace std;
 
 vector<int> parent;
-unordered_map<int, int> generator;
 
 int FindParent(int x) {
-    if (parent[x] != x)
-        parent[x] = FindParent(parent[x]);
-    return parent[x];
+    if (x == parent[x])
+        return x;
+    return parent[x] = FindParent(parent[x]);
 }
 void UnionParent(int a, int b) {
     int parentA = FindParent(a);
     int parentB = FindParent(b);
 
-    if (generator.count(parentA))
-        generator[parentB] = parentA;
-
-    else if (generator.count(parentB))
-        generator[parentA] = parentB;
-
-    else if (parentA < parentB)
+    if (parentA < parentB)
         parent[parentB] = parentA;
-
     else
-        parent[parentA] = parentB; 
+        parent[parentA] = parentB;
 }
-
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
 
     int n, m, k;
     cin >> n >> m >> k;
 
-    int num, u, v, w;
     vector<vector<int>> edges;
+    int generatorNum, u, v, w;
+
+    for (int i = 0; i <= n; i++)
+        parent.push_back(i);
 
     for (int i = 0; i < k; i++) {
-        cin >> num;
-        generator[num]++;    
+        cin >> generatorNum;
+        edges.push_back({0, 0, generatorNum});
     }
     for (int i = 0; i < m; i++) {
         cin >> u >> v >> w;
         edges.push_back({w, u, v});
     }
-    for (int i = 0; i <= n; i++)
-        parent.push_back(i);
-
     sort(edges.begin(), edges.end());
     int answer = 0;
 
     for (const auto& edge : edges) {
-        int parent1 = FindParent(edge[1]);
-        int parent2 = FindParent(edge[2]);
-
-        if (parent1 != parent2 && !(generator.count(parent1) && generator.count(parent2))) {
+        if (FindParent(edge[1]) != FindParent(edge[2])) {
             UnionParent(edge[1], edge[2]);
             answer += edge[0];
         }
