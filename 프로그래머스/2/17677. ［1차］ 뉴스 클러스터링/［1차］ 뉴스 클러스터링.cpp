738226c23011e035afa9e6f31f91d6798e049c01
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-05-25 19:11:07 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -20,7 +20,7 @@ void ToLower(string& word) {
             c = char(c + 'a' - 'A');
     }
 }
-void CreateComb(string& inputString, vector<string>& comb) {
+void CreateComb(string& inputString, int *arr) {
     string temp;
     for (int i = 0; i < inputString.size() - 1; i++) {
         temp = string(1, inputString[i]) + inputString[i + 1];
@@ -29,49 +29,24 @@ void CreateComb(string& inputString, vector<string>& comb) {
             continue;
         
         ToLower(temp);
-        comb.push_back(temp);
+        arr[(temp[0] - 'a') * 26 + (temp[1] - 'a')]++;
     }
 }
 int solution(string str1, string str2) {
     int answer = 0;
-    vector<string> comb1, comb2;
-
-    CreateComb(str1, comb1);
-    CreateComb(str2, comb2);
+    int c1[676] = {0,};
+    int c2[676] = {0,};
     
-    unordered_map<string, int> wordDict1, wordDict2;
-    vector<int> visited2(comb2.size(), 0);
+    CreateComb(str1, c1);
+    CreateComb(str2, c2);
+    
     int interCt = 0;
     int unionCt = 0;
     
-    for (int i = 0; i < comb1.size(); i++) {
-        wordDict1[comb1[i]]++;
-        for (int j = 0; j < comb2.size(); j++) {
-            if (visited2[j])
-                continue;
-            
-            if (comb1[i] == comb2[j]) {
-                interCt++;
-                visited2[j] = 1;
-                break;
-            }
-        }
+    for (int i = 0; i < 676; i++) {
+        interCt += min(c1[i], c2[i]);
+        unionCt += max(c1[i], c2[i]);
     }
-    for (int i = 0; i < comb2.size(); i++)
-        wordDict2[comb2[i]]++;
-    
-    for (auto [k, v] : wordDict2) {
-        if (wordDict1.count(k))
-            unionCt += max(wordDict1[k], v);
-        else
-            unionCt += v;
-    }
-    for (auto [k, v] : wordDict1) {
-        if (wordDict2.count(k))
-            continue;
-        else
-            unionCt += v;
-    }
-    answer = (interCt == 0 && unionCt == 0) ? 65536 : floor((double)interCt / unionCt * 65536);
+    answer = unionCt ? floor((double)interCt / unionCt * 65536) : 65536;
     return answer;
 }
