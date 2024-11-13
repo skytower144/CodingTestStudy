8f11fcfc7f83f2d9cb57c15398c8224b30e1b136
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-11-13 18:54:38 +0900

    [Gold V] Title: 탑, Time: 96 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> towers(n);
+	vector<pair<int, int>> stack;
+	vector<int> answers(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		int towerHeight;
+		cin >> towerHeight;
+
+		towers[i] = { towerHeight, i };
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int towerHeight = towers[i].first;
+
+		if (stack.empty())
+		{
+			stack.push_back({ towerHeight, i });
+			answers[i] = 0;
+			continue;
+		}
+		while (!stack.empty() && stack.back().first < towerHeight)
+			stack.pop_back();
+
+		int receivingTower = stack.empty() ? 0 : stack.back().second + 1;
+		answers[i] = receivingTower;
+		stack.push_back({ towerHeight, i });
+	}
+	for (int i = 0; i < n; i++)
+		cout << answers[i] << " ";
+
+	return 0;
+}
