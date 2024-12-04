446520f1a0b4d98eb5c14cec97e1270f64645c2f
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-05 00:23:32 +0900

    [Silver I] Title: 신입 사원, Time: 484 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -7,8 +7,8 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-	ios::sync_with_stdio(0), cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
 	int t;
 	cin >> t;
