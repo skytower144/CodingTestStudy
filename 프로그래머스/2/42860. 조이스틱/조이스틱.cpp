7540cea2516cf1f4485c232ae37269809ac5eff6
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-05 01:49:39 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -2,35 +2,64 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <climits>
 
 using namespace std;
 
-int GetAlphabetAmount(char target)
+int ChangeAlphabet(char target)
 {
-    int moveUp = target - 'A';
-    int moveDown = 'Z' - target + 1;
-    return min(moveUp, moveDown);
+    int up = target - 'A';
+    int down = 'Z' - target + 1;
+    return min(up, down);
 }
 
 int solution(string name) {
-    int answer = 0;
-    int nameSize = name.size();
-    int minMoveX = nameSize - 1;
+    int answer = INT_MAX;
+    int changeSum = 0;
     
-    for (int i = 0; i < nameSize; i++)
+    for (int i = 0; i < name.size(); i++)
     {
-        if (name[i] != 'A')
-            answer += GetAlphabetAmount(name[i]);
-    }
-    for (int i = 0; i < nameSize; i++)
-    {
-        int checkEndPos = i + 1;
-        while (checkEndPos < nameSize && name[checkEndPos] == 'A')
-            checkEndPos++;
+        if (name[i] == 'A')
+            continue;
         
-        minMoveX = min(minMoveX, (i * 2) + (1) + (nameSize - 1 - checkEndPos));
-        minMoveX = min(minMoveX, (nameSize - 1 - checkEndPos) * 2 + (2) + (i));
+        changeSum += ChangeAlphabet(name[i]);
+        
+        int sum = 0;
+        int index = i - 1;
+        int backForth = name.size() - i;
+        
+        while (index > 0 && name[index] == 'A')
+            index--;
+
+        sum += backForth * 2 + index;
+        
+        index = i + 1;
+        backForth = i;
+        
+        while (index < name.size() - 1 && name[index] == 'A')
+            index++;
+        
+        int distance = backForth * 2 + name.size() - index;
+        sum = min(sum, distance);
+        
+        if (answer > sum)
+            answer = sum;
     }
-    answer += minMoveX;
+    
+    int index2 = name.size() - 1;
+    
+    while (index2 > 0 && name[index2] == 'A')
+        index2--;
+    
+    cout << changeSum << endl;
+    cout << answer << endl;
+    
+    answer = min(answer, index2);
+    
+    if (answer == INT_MAX)
+        answer = 0;
+    
+    answer += changeSum;
+    
     return answer;
 }
