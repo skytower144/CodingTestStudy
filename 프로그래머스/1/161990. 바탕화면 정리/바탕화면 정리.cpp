1e829385279d7c39a5f88cf16acbb589e133940c
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-05 22:36:37 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -1,22 +1,31 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<string> wallpaper) {
-    int minX = wallpaper[0].size(), minY = wallpaper.size();
-    int maxX = 0, maxY = 0;
+    int width = wallpaper[0].size();
+    int height = wallpaper.size();
     
-    for (int y = 0; y < wallpaper.size(); y++) {
-        for (int x = 0; x < wallpaper[0].size(); x++) {
-            if (wallpaper[y][x] == '#') {
-                if (minX > x) minX = x;
-                if (minY > y) minY = y;
-                if (maxX < x + 1) maxX = x + 1;
-                if (maxY < y + 1) maxY = y + 1;
-            }
+    int minX = width - 1;
+    int minY = height - 1;
+    int maxX = 0;
+    int maxY = 0;
+    
+    for (int i = 0; i < wallpaper.size(); i++)
+    {
+        for (int j = 0; j < wallpaper[0].size(); j++)
+        {
+            if (wallpaper[i][j] == '.')
+                continue;
+            
+            minY = min(minY, i);
+            minX = min(minX, j);
+            
+            maxY = max(maxY, i);
+            maxX = max(maxX, j);
         }
     }
-    
-    return {minY, minX, maxY, maxX};
+    return {minY, minX, maxY + 1, maxX + 1};
 }
