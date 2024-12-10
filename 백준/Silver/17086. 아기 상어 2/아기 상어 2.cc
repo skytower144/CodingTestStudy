05b50e952a8d16c5f8a7b6451d4b8af17105898a
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-11 03:56:53 +0900

    [Silver II] Title: 아기 상어 2, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
@@ -9,20 +9,16 @@ using namespace std;
 const int MAX = 50 * 50 + 1;
 
 vector<vector<int>> grid;
-int dx[] = {  0, 0,-1, 1, -1,-1, 1, 1 };
+vector<vector<int>> dist;
+queue<pair<int, int>> q;
+
+int dx[] = { 0, 0,-1, 1, -1,-1, 1, 1 };
 int dy[] = { -1, 1, 0, 0, -1, 1,-1, 1 };
 int n, m;
+int answer = -1;
 
-int GetDistance(int x, int y)
+void BFS()
 {
-    vector<vector<int>> distance(n, vector<int>(m, MAX));
-    queue<pair<int, int>> q;
-
-    q.push({ x, y });
-    distance[y][x] = 0;
-
-    int minDistance = MAX;
-
     while (!q.empty())
     {
         pair<int, int> popped = q.front();
@@ -36,18 +32,15 @@ int GetDistance(int x, int y)
             if (nx < 0 || nx >= m || ny < 0 || ny >= n)
                 continue;
 
-            if (distance[ny][nx] <= distance[popped.second][popped.first] + 1)
+            if (dist[ny][nx] <= dist[popped.second][popped.first] + 1)
                 continue;
 
-            distance[ny][nx] = distance[popped.second][popped.first] + 1;
+            dist[ny][nx] = dist[popped.second][popped.first] + 1;
             q.push({ nx, ny });
 
-            if (grid[ny][nx] == 1)
-                minDistance = min(minDistance, distance[ny][nx]);
+            answer = max(answer, dist[ny][nx]);
         }
     }
-
-    return minDistance;
 }
 
 int main()
@@ -57,25 +50,23 @@ int main()
     cin >> n >> m;
 
     grid = vector<vector<int>>(n, vector<int>(m, 0));
-    int answer = -1;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-            cin >> grid[i][j];
-    }
+    dist = vector<vector<int>>(n, vector<int>(m, MAX));
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            if (grid[i][j] == 1)
-                continue;
+            cin >> grid[i][j];
 
-            answer = max(answer, GetDistance(j, i));
+            if (grid[i][j] == 1)
+            {
+                q.push({ j, i });
+                dist[i][j] = 0;
+            }
         }
     }
 
+    BFS();
     cout << answer;
 
     return 0;
