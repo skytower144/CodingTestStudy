c2d3c6cce5245f1ffe347dc49b64a41f1921eef2
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-13 04:37:47 +0900

    [Gold IV] Title: 스도쿠, Time: 176 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -6,8 +6,10 @@
 using namespace std;
 
 vector<vector<int>> sudoku(9, vector<int>(9, 0));
+vector<vector<bool>> existingRowNums(9, vector<bool>(10, false));
+vector<vector<bool>> existingColNums(9, vector<bool>(10, false));
+vector<vector<bool>> existingBoxNums(9, vector<bool>(10, false));
 vector<pair<int, int>> blanks;
-bool foundSolution = false;
 
 void Print()
 {
@@ -21,26 +23,15 @@ void Print()
 
 bool CanPlace(int x, int y, int num)
 {
-    for (int i = 0; i < 9; i++)
-    {
-        if (sudoku[i][x] == num || sudoku[y][i] == num)
-            return false;
-    }
+    // 만약 x번째 열에 해당 숫자가 존재한다면
+    if (existingColNums[x][num])
+        return false;
 
-    int startX = x / 3 * 3;
-    int startY = y / 3 * 3;
+    if (existingRowNums[y][num])
+        return false;
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            int currentX = startX + j;
-            int currentY = startY + i;
-
-            if (sudoku[currentY][currentX] == num)
-                return false;
-        }
-    }
+    if (existingBoxNums[y / 3 * 3 + x / 3][num])
+        return false;
 
     return true;
 }
@@ -49,25 +40,30 @@ void FindSolution(int idx)
 {
     if (idx == blanks.size())
     {
-        foundSolution = true;
         Print();
         exit(0);
     }
 
     int x = blanks[idx].first;
     int y = blanks[idx].second;
+    int boxIdx = y / 3 * 3 + x / 3;
     
     for (int k = 1; k <= 9; k++)
     {
         if (!CanPlace(x, y, k))
             continue;
 
-        if (foundSolution)
-            return;
-
         sudoku[y][x] = k;
+        existingRowNums[y][k] = true;
+        existingColNums[x][k] = true;
+        existingBoxNums[boxIdx][k] = true;
+
         FindSolution(idx + 1);
+
         sudoku[y][x] = 0;
+        existingRowNums[y][k] = false;
+        existingColNums[x][k] = false;
+        existingBoxNums[boxIdx][k] = false;
     }
 }
 
@@ -83,6 +79,12 @@ int main()
 
             if (sudoku[i][j] == 0)
                 blanks.push_back({ j, i });
+            else
+            {
+                existingRowNums[i][sudoku[i][j]] = true;
+                existingColNums[j][sudoku[i][j]] = true;
+                existingBoxNums[i / 3 * 3 + j / 3][sudoku[i][j]] = true;
+            }
         }
     }
 
