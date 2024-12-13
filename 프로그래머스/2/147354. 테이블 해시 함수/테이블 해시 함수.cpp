9ccc54b8cf20ccc33f482c5d2873750a5e948b92
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-13 18:04:28 +0900

    [level 2] Title: 테이블 해시 함수, Time: 6.56 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -12,17 +12,23 @@ bool compare(vector<int>& a, vector<int>& b)
     if (a[compIndex] != b[compIndex])
         return a[compIndex] < b[compIndex];
     
+    
     return a[0] > b[0];
 }
 
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     int answer = 0;
+    col -= 1;
     
-    compIndex = col - 1;
-    sort(data.begin(), data.end(), compare);
+    sort(data.begin(), data.end(), [col](auto& a, auto& b)
+         {
+             if (a[col] != b[col])
+                 return a[col] < b[col];
+             
+             return a[0] > b[0];
+         }
+    );
 
-    vector<int> modSums;
-    
     for (int i = row_begin - 1; i <= row_end - 1; i++)
     {
         int modSum = 0;
@@ -30,13 +36,8 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
         for (int j = 0; j < data[i].size(); j++)
             modSum += data[i][j] % (i + 1);
         
-        modSums.push_back(modSum);
+        answer ^= modSum;
     }
-    
-    answer = modSums[0];
-    
-    for (int i = 1; i < modSums.size(); i++)
-        answer ^= modSums[i];
     
     return answer;
 }
