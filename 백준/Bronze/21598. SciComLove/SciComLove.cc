76e777daf54c308e031fcdab06f549eee560c975
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-14 16:56:56 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cout << "SciComLove" << '\n';
+    
+    return 0;
+}
