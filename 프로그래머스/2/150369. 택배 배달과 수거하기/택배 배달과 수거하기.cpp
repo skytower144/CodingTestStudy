ae3ac1fbc42a7f5a6e4930bbd6df25cc56c18e62
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-22 18:23:25 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 6833.08 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -4,86 +4,42 @@
 
 using namespace std;
 
+int Unload(int j, int cap, vector<int>& loadList)
+{
+    int unloaded = 0;
+    
+    while (j >= 0 && unloaded <= cap)
+    {
+        if (unloaded + loadList[j] >= cap)
+        {
+            loadList[j] -= cap - unloaded;
+            unloaded = cap;
+            break;
+        }
+
+        unloaded += loadList[j];
+        loadList[j] = 0;
+        j--;
+    }
+    
+    return unloaded;
+}
+
 long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
     long long answer = 0;
-    int idx = n - 1;
     
-    while (idx >= 0)
+    for (int i = n - 1; i >= 0; i--)
     {
-        vector<int> dStack, pStack;
-        int dSum = 0, pSum = 0;
-        bool dFull = false;
-        bool pFull = false;
-        
-        bool isBreak = true;
-        
-        for (int i = idx; i >= 0; i--)
+        if (deliveries[i] > 0 || pickups[i] > 0)
         {
-            if (deliveries[i] != 0 || pickups[i] != 0)
-            {
-                isBreak = false;             
-                break;
-            }
+            Unload(i, cap, deliveries);
+            Unload(i, cap, pickups);
+            
+            answer += (i + 1) * 2;
         }
         
-        if (isBreak)
-            break;
-        
-        for (int i = idx; i >= 0; i--)
-        {
-            if (!dFull && deliveries[i] > 0)
-            {
-                if (dSum + deliveries[i] <= cap)
-                {
-                    dStack.push_back(i);
-                    dSum += deliveries[i];
-                    deliveries[i] = 0;
-                }
-                else
-                {
-                    dStack.push_back(i);
-                    deliveries[i] -= cap - dSum;
-                    dFull = true;
-                }
-            }
-
-            if (!pFull && pickups[i] > 0)
-            {
-                if (pSum + pickups[i] <= cap)
-                {
-                    pStack.push_back(i);
-                    pSum += pickups[i];
-                    pickups[i] = 0;
-                }
-                else
-                {
-                    pStack.push_back(i);
-                    pickups[i] -= cap - pSum;
-                    pFull = true;
-                }
-            }
-
-            if (dFull && pFull)
-                break;
-        }
-        
-        int farthest = 0;
-        int nextIdx = -1;
-        
-        if (!dStack.empty())
-        {
-            nextIdx = dStack.back();
-            farthest = dStack[0];
-        }
-        
-        if (!pStack.empty())
-        {
-            nextIdx = max(nextIdx, pStack.back());            
-            farthest = max(farthest, pStack[0]);
-        }
-        
-        answer += (farthest + 1) * 2;
-        idx = nextIdx;
+        if (deliveries[i] > 0 || pickups[i] > 0)
+            i++;
     }
     
     return answer;
