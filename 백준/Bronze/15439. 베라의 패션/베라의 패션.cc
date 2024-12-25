a8d2c39062f73e6fad360f1b9be41210b2b4419c
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-25 11:55:52 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	cout << n * (n - 1);
+
+	return 0;
+}
