7ffbabc2ed91e03a405f03d2776082c8cd1ed289
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-25 13:35:55 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.cc
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.cc
@@ -6,41 +6,25 @@
 
 using namespace std;
 
-
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	vector<int> mushrooms(10);
-	int totalSum = 0;
-	int answer = -1;
+	int answer = 0;
 
 	for (int i = 0; i < 10; i++)
 		cin >> mushrooms[i];
 	
 	for (int i = 0; i < 10; i++)
 	{
-		totalSum += mushrooms[i];
+		if (abs(answer - 100) < abs(answer + mushrooms[i] - 100))
+			break;
 
-		if (totalSum >= 100)
-		{
-			int diff1 = abs(totalSum - 100);
-			int diff2 = abs(totalSum - mushrooms[i] - 100);
-
-			if (diff1 <= diff2)
-				answer = totalSum;
-
-			else if (diff1 > diff2)
-				answer = totalSum - mushrooms[i];
-
-			cout << answer;
-			return 0;
-		}
+		answer += mushrooms[i];
 	}
 
-	answer = totalSum;
 	cout << answer;
-
 	return 0;
 }
