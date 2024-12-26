40ca1f92be278972f3fa5e853dfc07e2b26fa53d
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-26 19:33:03 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    unsigned long long n;
+    cin >> n;
+
+    unsigned long long result = n * n * n;
+
+    cout << n * n * n << '\n';
+    cout << 3;
+
+    return 0;
+}
