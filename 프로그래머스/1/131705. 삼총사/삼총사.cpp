fad0e6abad7d7ff53c7151fa889bada228a8bcf9
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-26 22:07:55 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -3,15 +3,21 @@
 
 using namespace std;
 
-int solution(vector<int> num) {
-    int answer = 0, numSize = num.size();
-    
-    for (int i = 0; i < numSize - 2; i++) {
-        for (int j = i + 1; j < numSize - 1; j++) {
-            for (int k = j + 1; k < numSize; k++) {
-                if (num[i] + num[j] + num[k] == 0) answer++;
-            }
-        }
+int answer = 0;
+
+void Comb(int index, int count, int sum, vector<int>& number)
+{
+    if (count == 3)
+    {
+        answer += (sum == 0);
+        return;
     }
+    
+    for (int i = index; i < number.size(); i++)
+        Comb(i + 1, count + 1, sum + number[i], number);
+}
+
+int solution(vector<int> number) {
+    Comb(0, 0, 0, number);
     return answer;
 }
