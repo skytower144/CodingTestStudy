bf3067265ae192f67d7b34005dc2992d3b48c0a7
skytower144 <98079532+skytower144@users.noreply.github.com>
2024-12-29 16:52:23 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(vector<int>& a, vector<int>& b)
+{
+    if (a[1] != b[1])
+        return a[1] > b[1];
+
+    if (a[2] != b[2])
+        return a[2] > b[2];
+
+    if (a[3] != b[3])
+        return a[3] > b[3];
+
+    return a[0] < b[0];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<vector<int>> info;
+    int country, gold, silver, bronze;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> country >> gold >> silver >> bronze;
+        info.push_back({ country, gold, silver, bronze });
+    }
+
+    sort(info.begin(), info.end());
+
+    int rank = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && info[i - 1][1] == info[i][1] && info[i - 1][2] == info[i][2] && info[i - 1][3] == info[i][3])
+        {
+            if (info[i][0] == k)
+                break;
+
+            continue;
+        }
+
+        rank++;
+
+        if (info[i][0] == k)
+            break;
+    }
+
+    cout << rank;
+
+    return 0;
+}
