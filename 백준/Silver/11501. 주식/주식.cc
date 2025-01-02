58e9221f34deb0d4506cad9cc98c92f22248813c
skytower144 <98079532+skytower144@users.noreply.github.com>
2025-01-02 20:20:09 +0900

    [Silver II] Title: 주식, Time: 660 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -1,42 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
-    int T, n;
-    cin >> T;
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
 
-    for (int t = 0; t < T; t++) {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
         cin >> n;
+
         vector<int> prices(n);
-        vector<pair<int, int>> priceInfo(n);
-        int currentIndex = 0;
-        long long profit = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             cin >> prices[i];
-            priceInfo[i].first = prices[i];
-            priceInfo[i].second = i;
+
+        vector<int> stack;
+        long long sum = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (!stack.empty() && stack[0] < prices[i])
+            {
+                for (int j = 1; j < stack.size(); j++)
+                    sum += stack[0] - stack[j];
+
+                stack.clear();
+            }
+
+            stack.push_back(prices[i]);
         }
-        sort(priceInfo.begin(), priceInfo.end());
 
-        while (!priceInfo.empty() || currentIndex < n) {
-            int maxPrice = priceInfo.back().first;
-            int maxIndex = priceInfo.back().second;
-            priceInfo.pop_back();
+        for (int j = 1; j < stack.size(); j++)
+            sum += stack[0] - stack[j];
 
-            if (currentIndex > maxIndex) continue;
-
-            for (int i = currentIndex; i < maxIndex; i++)
-                profit += maxPrice - prices[i];
-            
-            currentIndex = maxIndex + 1;
-        }
-        cout << profit << '\n';
+        cout << sum << '\n';
     }
 
     return 0;
