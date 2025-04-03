cb45386a5e3fcfbbb257eb78bfff2dd1bfcf4117
skytower144 <98079532+skytower144@users.noreply.github.com>
2025-04-04 00:25:30 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    cout << 2025 << '\n';
+    cout << "04" << '\n';
+    cout << "03";
+
+    return 0;
+}
